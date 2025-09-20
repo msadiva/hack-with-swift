edff8c010128477de79847e577daff22e6e57491
Sadiva Madaan <sadivamadaan@Sadivas-MacBook-Air.local>
2025-09-20 15:28:34 +0530

    checkpoint 3

--- File.swift
+++ File.swift
@@ -334,3 +334,23 @@ for i in 1..<5 {
 
     print (i)
 }
+
+//The problem is called fizz buzz, and has been used in job interviews, university entrance tests, and more for as long as I can remember. Your goal is to loop from 1 through 100, and for each number:If it’s a multiple of 3, print “Fizz”,
+//If it’s a multiple of 5, print “Buzz”
+//If it’s a multiple of 3 and 5, print “FizzBuzz”
+//Otherwise, just print the number.
+
+print ("Starting Fizz Buzz Game")
+
+for num in 1...20 {
+
+    if num.isMultiple(of: 3) && num.isMultiple(of: 5){
+        print ("FizzBuzz")
+    } else if num.isMultiple(of: 3){
+        print ("Fizz")
+    } else if num.isMultiple(of: 5){
+        print ("Buzz")
+    } else {
+        print (num)
+    }
+}
