46bcad7e608412a9d3e2dd7bf6f84c1378f3b6ed
Sadiva Madaan <sadivamadaan@192.168.1.18>
2025-10-06 11:33:39 +0530

    checkpoint 9

--- File5.swift
+++ File5.swift
@@ -174,3 +174,13 @@ let captains = [
 let new = captains["Serenity"] ?? "N/A"
 
 print (new)
+
+//Your challenge is this: write a function that accepts an optional array of integers, and returns one randomly. If the array is missing or empty, return a random number in the range 1 through 100.
+
+
+let arr : [Int] = []
+//let arr = [1,2,3,4,5] 
+
+let output = arr.randomElement() ?? Int.random(in : 1...100)
+
+print (output)
